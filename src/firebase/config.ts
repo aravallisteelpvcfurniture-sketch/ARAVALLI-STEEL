a0baee21af5fffe3bdf479a187248fb7eff82a62
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-07 09:58:51 +0000

    Google login krte hai to ye error aati kya problem hai banao or thik kro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:800070033021:web:60c723cac3755c523b5b14",
   "apiKey": "AIzaSyBc14g2SZsTxuF5CSqAuhLFtxtNyPjKBF4",
   "authDomain": "studio-3514832119-2a5d7.firebaseapp.com",
+  "storageBucket": "studio-3514832119-2a5d7.appspot.com",
   "measurementId": "",
   "messagingSenderId": "800070033021"
 };
